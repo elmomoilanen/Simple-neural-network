Fix numerical stability issues caused by cross entropy

simple_neural_network/evolution.py
@@ -94,14 +94,16 @@ class Evolution:
         self._lowest_col, self._highest_col, self._median_col = range(3)
 
         default_neurons = (
-            list(range(5, 50, 5)) + list(range(50, 225, 25)) + list(range(250, 1250, 250))
+            list(range(5, 50, 5)) + list(range(50, 225, 25)) + list(range(250, 2000, 250))
         )
 
         self._activations = ANN.allowed_hidden_activations
         self._optimizers = ANN.allowed_optimizers
         self._neurons = tuple(kwargs.get("neurons", default_neurons))
-        self._learning_rates = tuple(kwargs.get("learning_rates", (1e-3, 1e-2, 0.1, 0.5, 1.0)))
-        self._lambdas = tuple(kwargs.get("lambdas", (0.0, 5.0, 25.0, 50.0)))
+        self._learning_rates = tuple(
+            kwargs.get("learning_rates", (1e-3, 1e-2, 0.1, 0.5, 1.0, 5.0, 10.0, 25.0))
+        )
+        self._lambdas = tuple(kwargs.get("lambdas", (0.0, 1.0, 2.5, 5.0, 10.0, 25.0, 50.0, 100.0)))
 
         self._param_set = {
             "hidden_nodes": self._neurons,
@@ -225,7 +227,6 @@ class Evolution:
     @staticmethod
     def _remove_weights_file(file_path):
         if not os.path.isfile(file_path):
-            logger.warning(f"Not a file or cannot find `{file_path}`")
             return
 
         os.unlink(file_path)
@@ -268,6 +269,7 @@ class Evolution:
         method_type: str,
         early_stop_threshold: int = 25,
         epochs: int = 50,
+        batch_size: int = 32,
         use_validation: bool = True,
     ) -> List[Dict]:
         """Run evolution based hyperparameter optimization.
@@ -296,6 +298,10 @@ class Evolution:
             Count of total feedforward/backpropagation passes through the network.
             Default value is 50.
 
+        batch_size : int
+            Count of training data in one iteration. Default is 32 which would mean
+            ceil(X.shape[0] / 32) iterations for every epoch.
+
         use_validation : bool
             Whether to use separate validation data in neural network fitting.
             True as default.
@@ -319,6 +325,7 @@ class Evolution:
 
         early_stop_thres = int(early_stop_threshold)
         epochs = int(epochs)
+        batch_size = int(batch_size)
         use_validation = bool(use_validation)
 
         weights_save_path = os.path.join(os.path.split(__file__)[0], "_evo_weights.h5")
@@ -335,7 +342,6 @@ class Evolution:
             logger.info(f"Evolution generation: {gener}/{self.generations}")
 
             for iter, param_set in enumerate(population):
-                logger.info(f"Hyperparameter set {iter+1}/{len(population)}")
                 # ANN cannot handle argument "fitness"
                 param_set.pop("fitness", None)
 
@@ -343,34 +349,41 @@ class Evolution:
                     **param_set,
                     method=method_type,
                     early_stop_threshold=early_stop_thres,
-                    verbose_level="mid",
+                    verbose_level=None,
                 )
                 ann.fit(
                     x_train,
                     y_train,
                     epochs=epochs,
-                    batch_size=32,
+                    batch_size=batch_size,
                     weights_save_path=weights_save_path,
                     use_validation=use_validation,
                 )
+                time.sleep(0.5)
+
                 try:
                     y_pred = ann.predict(x_test, weights_path=weights_save_path)
-                except FileNotFoundError as err:
-                    logger.warning(err)
-                    # assuming cost is used as fitness, +inf is the worst then
+                except FileNotFoundError:
+                    # assuming cost is used as a fitness score, then +inf is the worst result
                     param_set["fitness"] = np.inf
-                    continue
+                else:
+                    param_set["fitness"] = eval_cost(
+                        y_test.reshape(-1), y_pred.reshape(-1), method=method_type
+                    )
 
-                param_set["fitness"] = eval_cost(
-                    y_test.reshape(-1), y_pred.reshape(-1), method=method_type
+                logger.info(
+                    f"Hyperset {iter+1}/{len(population)}: Fitness value (cost) for test data: {param_set['fitness']:.2f}"
                 )
-                logger.info(f"Fitness value (cost) for test data: {param_set['fitness']:.2f}")
 
             self._remove_weights_file(weights_save_path)
             self._save_generation_fitness_score(gener - 1, population)
 
             if gener == self.generations:
                 return sorted(population, key=lambda x: x["fitness"])
+
+            logger.info(
+                f"Best hyperset so far: {sorted(population, key=lambda x: x['fitness'])[0]}"
+            )
 
             new_population = self._select_by_fitness(population)
             self._mutate(new_population)

simple_neural_network/metrics.py
@@ -80,10 +80,10 @@ def eval_cost(y_true: np.ndarray, y_pred: np.ndarray, method: str) -> float:
 
         # don't allow zero or small negative return values
         # e.g. if `matches` array contains only ones, the return value would be slightly negative
-        return max(-np.mean(log_y), 1e-20)
+        return max(-np.mean(log_y), 1e-12)
 
     if method not in ("reg", "regression"):
         raise ValueError(f"Cannot evaluate cost for learning type `{method}`")
 
     square_of_err = (y_true - y_pred) ** 2
-    return max(np.mean(square_of_err), 1e-20)
+    return max(np.mean(square_of_err), 1e-12)

simple_neural_network/neural_network.py
@@ -88,9 +88,11 @@ class ANN:
         determines whether validation is used at all. Thus, setting it to False overrides
         this validation size (becomes effectively zero).
 
-    verbose_level : str
-        Control amount of logging entries. Accepted values are "high", "mid" and "low".
-        Default is "low" which will log every 50th epoch during the fitting process.
+    verbose_level : Optional[str]
+        Controls the amount of logging entries. Accepted level names are "high",
+        "mid" and "low". Level None would suppress all logging. For level "high",
+        every epoch would be logged. Default level is "low" which logs every 50th
+        epoch during the fitting process.
 
     random_seed: Optional[int]
         Seed used to initialize random number generator. Defaults to None.
@@ -126,7 +128,7 @@ class ANN:
         activation1: str = "relu",
         activation2: str = "relu",
         validation_size: float = 0.2,
-        verbose_level: str = "low",
+        verbose_level: Optional[str] = "low",
         random_seed: Optional[int] = None,
     ) -> None:
         self.hidden_nodes = hidden_nodes
@@ -318,28 +320,19 @@ class ANN:
 
     @staticmethod
     def _cross_entropy(y_inv, y_pred):
-        # y_inv[i] is the correct y inv value for column i (and only value equal to one)
-        # filter out probabilities (y_pred values) for which y is zero
-        non_zero_y = y_pred[y_inv, np.arange(y_pred.shape[1])]
-        log_y = np.log(non_zero_y + 1e-9)
+        labels = y_inv.shape[0]
+        y_pred = np.clip(y_pred, 1e-12, 1.0 - 1e-12)
 
-        return max(-np.mean(log_y), 1e-20)
+        log_likelihood = -np.log(y_pred[y_inv, range(labels)])
+        return np.sum(log_likelihood) / labels
 
     @staticmethod
     def _dcross_entropy(y_inv, y_pred):
-        cols = np.arange(y_pred.shape[1])
+        labels = y_inv.shape[0]
+        dmatrix = y_pred
 
-        non_zero_y = y_pred[y_inv, cols]
-        non_zero_y_inv = np.zeros(non_zero_y.shape, dtype=non_zero_y.dtype)
-
-        pos_mask = non_zero_y > 0
-
-        if pos_mask.sum() > 0:
-            non_zero_y_inv[pos_mask] = -1.0 / non_zero_y[pos_mask]
-            non_zero_y_inv[pos_mask] /= y_inv.shape[0]
-
-        dmatrix = np.zeros_like(y_pred)
-        dmatrix[y_inv, cols] = non_zero_y_inv
+        dmatrix[y_inv, range(labels)] -= 1
+        dmatrix /= labels
 
         return dmatrix
 
@@ -348,7 +341,7 @@ class ANN:
         square_of_err = (y.T - y_pred) ** 2
         mse = np.mean(square_of_err) * 0.5
 
-        return max(mse, 1e-20)
+        return max(mse, 1e-12)
 
     @staticmethod
     def _dmse(y, y_pred):
@@ -387,9 +380,9 @@ class ANN:
         ss_resid = np.sum((y_inv.T - y_pred) ** 2)
 
         if ss_total < 1e-6:
-            return 1e-20
+            return 1e-15
 
-        return max(1.0 - (ss_resid / ss_total), 1e-20)
+        return max(1.0 - (ss_resid / ss_total), 1e-12)
 
     def _compute_predict(self, X):
         h1_pred = self._afn1(np.matmul(self._w["w1"], X.T) + self._b["b1"])
@@ -611,13 +604,11 @@ class ANN:
     def _log_status(self, epoch, elapsed_time, log_type="train"):
         print_log = False
 
-        if epoch == self._epochs:
+        if self._verbose_level == "high":
             print_log = True
-        elif self._verbose_level == "high":
+        elif self._verbose_level == "mid" and (epoch % 10 == 0 or epoch == self._epochs):
             print_log = True
-        elif self._verbose_level == "mid" and epoch % 10 == 0:
-            print_log = True
-        elif epoch % 50 == 0:
+        elif self._verbose_level == "low" and (epoch % 50 == 0 or epoch == self._epochs):
             print_log = True
 
         if log_type == "train" and print_log:
@@ -645,9 +636,10 @@ class ANN:
     ) -> None:
         """Fit the neural network.
 
-        Epoch is one total pass of all the training data, batch size determines
-        the amount of training data for one pass. For n observations (X.shape[0]),
-        there are ceil(n / batch_size) iterations in one total pass.
+        Epoch is one total pass of all the training data, which may consists of one
+        or more iterations. Batch size determines the amount of iterations for each epoch.
+        If the data X has n observations (i.e., X.shape[0]), there are ceil(n / batch_size)
+        iterations in one epoch.
 
         Parameters
         ----------
@@ -662,10 +654,11 @@ class ANN:
             Default value is 100.
 
         batch_size : Optional[int]
-            Count of training data in single pass, the default count is X.shape[0].
-            Default is used when the argument is given as None. For batch size k
-            (k <= X.shape[0]), there will be ceil(X.shape[0] / k) iterations in
-            single epoch. The smallest allowed batch size is one.
+            Amount of training data in one iteration, default size is X.shape[0] which
+            means that each epoch contains only one iteration. Default size is used when
+            the argument is given as None. For an integer batch size k (k <= X.shape[0]),
+            there will be ceil(X.shape[0] / k) iterations in single epoch. The smallest
+            allowed batch size is one.
 
         use_validation : bool
             Default value True means that a separate validation data is created
@@ -728,8 +721,6 @@ class ANN:
         no_upgrade_counter = 0
         start_timestamp = time.perf_counter()
 
-        logger.info(f"Begin training, do {self._iters} iterations for every {self._epochs} epochs")
-
         for epoch in range(1, self._epochs + 1):
             epoch_rows = self._rng.choice(x_indices, size=len(x_indices), replace=False)
             X, y_inv = X[epoch_rows], y_inv[epoch_rows]
@@ -763,7 +754,7 @@ class ANN:
                     no_upgrade_counter = 0
                 else:
                     no_upgrade_counter += 1
-                    if no_upgrade_counter >= self._early_stop_thres:
+                    if no_upgrade_counter >= self._early_stop_thres and self._verbose_level:
                         logger.info(
                             f"Early stop threshold {self._early_stop_thres} reached, stop training at epoch {epoch}"
                         )
@@ -776,13 +767,13 @@ class ANN:
                     no_upgrade_counter = 0
                 else:
                     no_upgrade_counter += 1
-                    if no_upgrade_counter >= self._early_stop_thres:
+                    if no_upgrade_counter >= self._early_stop_thres and self._verbose_level:
                         logger.info(
                             f"Early stop threshold {self._early_stop_thres} reached, stop training at epoch {epoch}"
                         )
                         break
 
-        if self._weights_save_epoch and self._weights_save_epoch > 0:
+        if self._weights_save_epoch and self._weights_save_epoch > 0 and self._verbose_level:
             logger.info(f"Weights last saved at epoch {self._weights_save_epoch}")
 
         self._stopping_epoch = epoch

tests/test_neural_network.py
@@ -288,12 +288,23 @@ def test_cross_entropy_derivative():
 
     # y_pred must be 3 x 4
     y_pred = np.array([[0.25, 0.0, 0.7, 0.5], [0.5, 0.0, 0.15, 0.5], [0.25, 1.0, 0.15, 0.0]])
-    zero_mask = y_pred == 0.0
 
     dcross_entr = ANN._dcross_entropy(y_inv, y_pred)
     assert dcross_entr.shape == y_pred.shape
 
-    assert np.alltrue(dcross_entr[zero_mask] == 0.0)
+    assert np.alltrue(
+        np.isclose(
+            dcross_entr,
+            np.array(
+                [
+                    [-0.1875, 0.000, 0.175, 0.125],
+                    [0.125, -0.250, -0.2125, 0.125],
+                    [0.0625, 0.250, 0.0375, -0.250],
+                ]
+            ),
+            atol=0.001,
+        )
+    )
 
 
 def test_mse():